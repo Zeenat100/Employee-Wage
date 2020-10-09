31e460e509cf06b002e0bfa05f5afbc6cfabffb3
Zeenat100 <siddzee100@gmail.com>
2020-10-09 00:37:20 -0700

    Add Emp wage switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,16 +6,16 @@ is_full_time=2
 
 empCheck=$((RANDOM%3));
 
-if [ $is_full_time -eq $empCheck ]
-then
-        echo "Employee is Present";
-        emp_hrs=8;
-elif [ $is_part_time -eq $empCheck ]
-then
-	emp_hrs=4;
-else
-	echo "Employee is absent";
-	emp_hrs=0;
-fi
+case $empCheck in
+	$is_full_time)
+        	emp_hrs=8
+		;;
+	$is_part_time)
+		emp_hrs=4
+		;;
+	*)
+	emp_hrs=0
+		;;
+esac
 emp_wage=$(($emp_hrs * $emp_wage_per_hr))
 echo "Employee wage is "$emp_wage
