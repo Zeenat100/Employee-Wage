38a7c815b70c51679e47ec5f122cf6402c9b1953
Zeenat100 <siddzee100@gmail.com>
2020-10-09 14:07:29 -0700

    Add function for employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation";
+
 emp_wage_per_hr=20;
 is_part_time=1
 is_full_time=2
@@ -8,12 +9,9 @@ total_salary=0
 max_hrs_month=100
 total_emp_hrs=0
 total_working_days=0
-while [[ $total_emp_hrs -lt $max_hrs_month &&
-	$total_working_days -lt $num_working_days ]]
-do
-	((total_working_days++))
-	empCheck=$((RANDOM%3))
-	case $empCheck in
+
+function getWorkHours() {
+	case $1 in
 		$is_full_time)
         		emp_hrs=8
 			;;
@@ -24,6 +22,12 @@ do
 			emp_hrs=0
 			;;
 	esac
+}
+while [[ $total_emp_hrs -lt $max_hrs_month &&
+        $total_working_days -lt $num_working_days ]]
+do
+        ((total_working_days++))
+        getWorkHours $((RANDOM%3))
 	total_emp_hrs=$(($total_emp_hrs + $emp_hrs))
 done
 total_salary=$(($total_emp_hrs * $emp_wage_per_hr))
